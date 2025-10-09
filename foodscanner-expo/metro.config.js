d6e56dc03a880e75db9861c32f0e416682d0e832
spamhws <egorsanko@gmail.com>
2025-10-09 08:47:58 -0400

    m

---
+++ foodscanner-expo/metro.config.js
@@ -0,0 +1,13 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname);
+
+// Fix for React Query .js extension resolution
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
+config.resolver.unstable_enablePackageExports = true;
+
+module.exports = withNativeWind(config, {
+  input: './global.css',
+  configPath: './tailwind.config.js',
+});
